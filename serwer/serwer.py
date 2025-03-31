1f97e2043b7c7fe3e74b43b9af71049acf8d5cad
Dawid <dawid_03@onet.eu>
2025-04-01 00:01:40 +0200

    add PhotoButton, MixedButton and functions of clip and muting on discord

--- serwer/serwer.py
+++ serwer/serwer.py
@@ -4,14 +4,15 @@ import keyboard
 from flask_cors import CORS  
 import psutil
 import pygetwindow as gw
+import win32gui
 
 app = Flask(__name__)
-CORS(app)  # Dodaje wsparcie dla CORS
+CORS(app)
 
 @app.route('/desktop', methods=['POST'])
 def desktop():
     try:
-        keyboard.press_and_release('win+d')  # Symulacja naciśnięcia Win + D
+        keyboard.press_and_release('win+d')
         return jsonify({"message": "Win+D pressed: Minimizing all windows"}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
@@ -19,7 +20,7 @@ def desktop():
 @app.route('/mute', methods=['POST'])
 def mute():
     try:
-        subprocess.run(["nircmd.exe", "mutesysvolume", "2"], check=True)  # Użycie nircmd do wyciszenia
+        keyboard.press_and_release('ctrl+shift+m')
         return jsonify({"message": "Toggled mute"}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
@@ -34,12 +35,28 @@ def clip():
         return jsonify({"error": str(e)}), 500
 
 @app.route('/getApp', methods=['GET'])
-def getApps():
-    windows = gw.getWindowsWithTitle('')
-    active_processes = [window.title for window in windows if window.title]
-    print("Procesy na pasku zadań:")
-    for title in active_processes:
-        print(f"- {title}")
+def get_active_processes():
+    def get_window_pid(hwnd):
+        """Zwraca PID procesu dla danego uchwytu okna"""
+        _, pid = win32gui.GetWindowThreadProcessId(hwnd)
+        return pid
+
+    windows = []
+    def enum_window_callback(hwnd, _):
+        if win32gui.IsWindowVisible(hwnd): 
+            title = win32gui.GetWindowText(hwnd)
+            if title:
+                pid = get_window_pid(hwnd)
+                windows.append((title, pid))
+
+    win32gui.EnumWindows(enum_window_callback, None)
+
+    processes = {p.pid: p.name() for p in psutil.process_iter(['pid', 'name'])}
+
+    print("Aplikacje na pasku zadań:")
+    for title, pid in windows:
+        process_name = processes.get(pid, "Unknown")
+        print(f"{process_name} (PID: {pid}) - {title}")
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)  # Nasłuchuj na wszystkich interfejsach
+    app.run(host="0.0.0.0", port=5000)
