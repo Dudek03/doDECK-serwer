2883a64ce801bc9ec013d00e02181ef63927c9c4
Dawid <dawid_03@onet.eu>
2026-03-01 22:53:43 +0100

    change cpu and ram usage to one func

--- modules/system.py
+++ modules/system.py
@@ -5,17 +5,10 @@ system_bp = Blueprint("system", __name__)
 
 
 @system_bp.route("/getCPUUsage", methods=["GET"])
-def getCPUUsage():
+def getUsageData():
     try:
         cpuUsage = psutil.cpu_percent(interval=0.1, percpu=True)
-        return jsonify({"cpu usage": max(cpuUsage)}), 200
-    except Exception as e:
-        return jsonify({"error message: ": str(e)}), 500
-
-
-@system_bp.route("/getRAMUsage", methods=["GET"])
-def getRAMUsage():
-    try:
-        return jsonify({"RAM usage": psutil.virtual_memory().percent}), 200
+        ramUsage = psutil.virtual_memory().percent
+        return jsonify({"cpu": max(cpuUsage), "ram": ramUsage}), 200
     except Exception as e:
         return jsonify({"error message: ": str(e)}), 500
