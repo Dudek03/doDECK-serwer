ed339d8ed93bccee15ca6be2a9e828ba1cfaeb72
Dawid <dawid_03@onet.eu>
2026-03-03 22:36:19 +0100

    fix missing blueprint

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,6 +1,6 @@
 from .audio import audio_bp
-from .discord import discord_bp
 from .imageHandler import image_bp
 from .system import system_bp
+from .dispatcher import dispatcher_bp
 
-blueprints = [audio_bp, discord_bp, image_bp, system_bp]
+blueprints = [audio_bp, image_bp, system_bp, dispatcher_bp]
